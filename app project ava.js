d1d479459ad4547ef3e0ee0b6aa9ac17af6d2323
avahoddy <117802552+avahoddy@users.noreply.github.com>
2022-12-14 11:47:41 -0700

    Add files via upload

---
+++ app project ava.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>repl.it</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="nav"></div>
+    <div id="contentContainer"></div>
+        <h1>My Fancy Website</h1>
+        <p>kjedksdknfl daskflkjsa fdkdsf sakdfjlkds salflalfdsjlajs fdas dfasjf asdgasudfsiudhf saidfuhuf ladsfla daklsdjfd fsdjaslf afjakdfajskldfj askdjf sakjdfhdsa fdjadhfsajdfdla fdaldfka kjflkdsf akfjklsdfkdaslk fsajdkf dskflkadfj asj f salkdflakskfksjdflaj dfkajfka fkjsafkjsakf kdakjfkdjflkjdsafksajfdkldsajfkla dfjlakfjkdsjklfjkjf ljaskljfd safajfldkjf sakdjfksajdksjfkldsjf</p>
+        <p>jdkhfjhga adfkjdahfdhjsdf sfjeijawjef kdnckznckaeifjadkcc sdkasjfowaef dkclkja fk fsjfjfief lkdlfijaejf lskdlfajlf slfjieff kdfafkdsjfjoief fkdkajfljfief sdkljfliefaelfkckjfa fkjfiieksc alkfnejawh fjisdjfk jfielskkf ejfdkjflaeijf dkljfaljiejfakdf salijeefjaflskdfj iejjflkdjfiae kadfaoelfk</p>
+        <ul>
+            <li>An even listt item</li>
+            <li>An odd list item</li>
+            <li>An even list item</li>
+            <li>An odd listt item</li>
+            <li>An even list item</li>
+            <li>An odd listt item</li>
+            <li>An even list item</li>
+            <li>An odd listt item</li>
+            <li>An even list item</li>
+            <li>An odd listt item</li>
+            <li>An even list item</li>
+        </ul>
+        <label for="myInput">First Name: </label>
+        <input id="myInput" placeholder="Ava" type="text"></input>
+  
+  $().somejQueryFunction();  
+  let h1 = (app.js);
+</div>
+</body>
+</html>
+
+  <
